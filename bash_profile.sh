56fe849976c75cccc8518165f3ee8058210aa5b6
Alan L. V. Guedes <alan@telemidia.puc-rio.br>
2018-12-07 19:17:42 -0200

    add mac_install_refind

--- bash_profile.sh
+++ bash_profile.sh
@@ -143,6 +143,9 @@ function hf_mac_install_bash4(){
   echo $BASH && echo $BASH_VERSION
 }
 
+function hf_mac_install_refind(){
+  sudo apt install refind
+}
 
 # ---------------------------------------
 # audio functions
@@ -642,10 +645,8 @@ function hf_user_passwd_disable_len_restriction() {
 
 function hf_user_permissions_opt() {
     sudo chown root:root /opt
-    # if ! (id -nG "$USER" | grep -qw root); then
     sudo adduser $USER root
     newgrp root # update group for user
-    # fi
     sudo chmod -R +775 /opt/
 }
 
